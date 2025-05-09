8e8d0b29f62744ff968ad94e13a171d8d2492d96
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-05-09 18:33:55 +0700

    add rodording todo form

--- custom-addons/odoo18-module-development/addons/Todo-list/models/todolist.py
+++ custom-addons/odoo18-module-development/addons/Todo-list/models/todolist.py
@@ -1,7 +1,22 @@
 from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from datetime import datetime
+    
+class TodoTag(models.Model):
+    _name = 'todo.tag'
+    _description = 'Todo Tags'
 
+    name = fields.Char(string='Tag Name', required=True)
+    color = fields.Integer(string='Color Index')
+
+    @api.model
+    def _setup_default_tags(self):
+        default_tags = ['Work', 'Event', 'Life']
+        for tag_name in default_tags:
+            existing_tag = self.search([('name', '=', tag_name)])
+            if not existing_tag:
+                self.create({'name': tag_name})
+    
 class TodoList(models.Model):
     _name = 'todo.list'
     _description = 'Todo List'
@@ -17,11 +32,17 @@ class TodoList(models.Model):
         ('incomplete', 'Incomplete'),
         ('complete', 'Complete')
     ], string='Status', default='draft', tracking=True)
-    tags = fields.Selection([
-        ('work', 'Work'),
-        ('event', 'Event'),
-        ('life', 'Life')
-    ], string='Tags', default='work', tracking=True)
+    tag_ids = fields.Many2many(
+        'todo.tag',
+        string='Tags',
+    )
     attendee_ids = fields.Many2many(comodel_name='res.users', string='Attendees')
     
-    #FIXME - end date must be after start date
+    @api.constrains('start_date', 'end_date')
+    def _check_dates(self):
+        for record in self:
+            if record.start_date and record.end_date:
+                if record.start_date > record.end_date:
+                    raise ValidationError(_('End date must be after start date.'))
+    
+    
