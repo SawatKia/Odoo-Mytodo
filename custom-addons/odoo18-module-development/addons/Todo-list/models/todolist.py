6fa325c9bdc92a6c1fa5ea46efee04258b873cef
Sawat Lapprasertlum <sawat.kia2000@gmail.com>
2025-05-09 17:36:51 +0700

    remove todotags model

--- custom-addons/odoo18-module-development/addons/Todo-list/models/todolist.py
+++ custom-addons/odoo18-module-development/addons/Todo-list/models/todolist.py
@@ -2,25 +2,6 @@ from odoo import models, fields, api, _
 from odoo.exceptions import ValidationError
 from datetime import datetime
 
-class TodoTag(models.Model):
-    _name = 'todo.tag'
-    _description = 'Todo Tag'
-
-    tag_name = fields.Selection([
-        ('work', 'Work'),
-        ('event', 'Event'),
-        ('life', 'Life'),
-        ('achievement', 'Achievement')], string='Tag')
-    todo_list_id = fields.Many2one(comodel_name='todo.list', string='Todo List')  # Add this field
-    # user_id = fields.One2Many(comodel_name='res.users', inverse_name=)
-
-    # _sql_constraints = [
-    #     ('unique_name_per_user', 'unique(name, user_id)', 'Tag name must be unique per user!')
-    # ]
-
-    #FIXME - add ability to add more tags
-
-
 class TodoList(models.Model):
     _name = 'todo.list'
     _description = 'Todo List'
@@ -36,7 +17,11 @@ class TodoList(models.Model):
         ('incomplete', 'Incomplete'),
         ('complete', 'Complete')
     ], string='Status', default='draft', tracking=True)
-    tag_ids = fields.One2many(comodel_name='todo.tag', inverse_name='todo_list_id', string='Tags')  # Use the new field as inverse_name
+    tags = fields.Selection([
+        ('work', 'Work'),
+        ('event', 'Event'),
+        ('life', 'Life')
+    ], string='Tags', default='work', tracking=True)
     attendee_ids = fields.Many2many(comodel_name='res.users', string='Attendees')
     
     #FIXME - end date must be after start date
